425355a1fd04c507ac9de90e5c76d984740c9ab9
workandwit-admin <workandwit@gmail.com>
2026-09-01 23:13:44 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/global.css'
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, '')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
